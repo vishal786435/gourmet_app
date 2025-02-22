3f065e02f1e141aa9c30b4379891338dbc9bec93
JWuerdz <144745637+JWuerdz@users.noreply.github.com>
2025-02-21 19:09:31 -0500

    Update server-background.js

--- .netlify/functions/server-background.js
+++ .netlify/functions/server-background.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const mongoClient = new MongoClient(process.env.MONGODB_URI || "https://teams.microsoft.com/l/message/19:7f1876a3-eae7-4713-8f15-f394f4eb92af_8238b593-d1a3-4438-93c8-b6be3563d7de@unq.gbl.spaces/1740182457911?context=%7B%22contextType%22%3A%22chat%22%7D");
+const mongoClient = new MongoClient(process.env.MONGODB_URI || "mongodb+srv://admin:admin@gourmet2go.5jpsj.mongodb.net/");
 
 const clientPromise = mongoClient.connect();
 
